78fb1875fe855956f6c8b8aa1be67be798433cb6
zhangonmacbook <zhangmdev@gmail.com>
2013-04-03 02:21:34 +0800

    e3 sparse t

--- eg3/main.cpp
+++ eg3/main.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
 using Eigen::MatrixXd;
+typedef Eigen::SparseMatrix<double> SparseMatrixType;
+
+void testSparse()
+{
+    std::cout<<"test sparse\n";
+    int n = 16;
+    SparseMatrixType Rs(n, n);
+    Rs.reserve(Eigen::VectorXi::Constant(n,3));
+    for( int i = 0; i < n; i++ ) {
+        Rs.insert(i,i) = 1.f;
+        
+        if(i> 0)Rs.insert(i,i-1) = -.5f;
+        else Rs.insert(i,4) = -.5f;
+        
+        if(i< n-1)Rs.insert(i,i+1) = -.5f;
+        else Rs.insert(i,3) = -.5f;
+    }
+    
+    Rs.makeCompressed(); 
+    std::cout << "Rs \n" << Rs << std::endl;
+    
+    Eigen::VectorXd b(n);
+    b.setRandom();
+    std::cout << "b \n" <<b<<"\n";
+    
+    Eigen::SimplicialLDLT<SparseMatrixType> chol(Rs);
+    
+    std::cout<<"U\n"<<chol.matrixU()<<"\n";
+    std::cout<<"L\n"<<chol.matrixL()<<"\n";
+    
+    Eigen::VectorXd x = chol.solve(b); 
+    std::cout << "x \n" <<x<<"\n";
+}
+
 int main()
 {
-MatrixXd m(2,2);
-m(0,0) = 3;
-m(1,0) = 2.5;
-m(0,1) = -1;
-m(1,1) = m(1,0) + m(0,1);
-std::cout << m << std::endl;
+    testSparse();
+    return 0;
 }
