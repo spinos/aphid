601fb75e3dcc166d7d5a6d94b7332159339c06db
zhang at office <zhangjian@.of3d.com>
2013-09-04 17:56:29 +0800

    m

--- shared/CircleCurve.cpp
+++ shared/CircleCurve.cpp
@@ -18,10 +18,8 @@ void CircleCurve::create()
     m_numVertices = 37;
     m_cvs = new Vector3F[numVertices()];
     const float delta = PI / 18.f;
-    for(int i = 0; i <= 36; i++) {
+    for(int i = 0; i <= 36; i++)
 		m_cvs[i].set(sin(delta * i), cos(delta * i), 0.f);
-		printf("i %i", i);
-	}
 	
 	m_length = 0;
 	for(unsigned i = 1; i < numVertices(); i++) {
