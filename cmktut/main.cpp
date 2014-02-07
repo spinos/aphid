db9431d8a50889157808216caa9a6a4395792339
zhang on win7 <Administrator@.(none)>
2014-02-07 16:58:10 +0800

    cmk tut

--- cmktut/main.cpp
+++ cmktut/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "TutorialConfig.h"
-
+#include <hdf5.h>
 int main (int argc, char * const argv[]) {
     // insert code here...
     std::cout << "Hello, CMake!\n";
-    std::cout<<"CMake Tutorial Version "<<Tutorial_VERSION_MAJOR<<
-            "."<<Tutorial_VERSION_MINOR;
-	return 0;
+    std::cout<<"CMake Tutorial Version "<<Tutorial_VERSION_MAJOR<<"."<<Tutorial_VERSION_MINOR;
+    std::cout<<"HDF5 test\n";
+    hid_t fFileId = H5Fcreate("dest.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    std::cout<<"file id is "<<fFileId;
+    return 0;
 }
