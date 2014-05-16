8b71bbf6f61450c955f19098761a06e06f60ca19
zhang at office <zhangjian@.of3d.com>
2014-05-16 18:16:49 +0800

    m

--- btree/Sculptor.cpp
+++ btree/Sculptor.cpp
@@ -288,6 +288,11 @@ void Sculptor::pinchPoints()
 	movePointsToward(m_active->meanPosition, 0.02f);
 }
 
+void Sculptor::spreadPoints()
+{
+    movePointsToward(m_active->meanPosition, -0.02f);
+}
+
 void Sculptor::smudgePoints(const Vector3F & x)
 {
 	movePointsAlong(x, 0.09f);
@@ -319,7 +324,7 @@ void Sculptor::smoothPoints()
 			
 			m_topo->getDifferentialCoord(vert.key, d);
 			
-			pos += d * 0.02f * m_active->weight(vi) * m_strength;
+			pos -= d * 0.1f * m_active->weight(vi) * m_strength;
 		
 			m_tree->displace(vert, p0);
 			vi++;
