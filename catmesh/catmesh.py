2bebab4e42a8b3a825b2aed0f25e2e9bc52649ad
zhangonmacbook <zhangmdev@gmail.com>
2014-06-10 05:15:12 +0800

    a circuit

--- catmesh/catmesh.py
+++ catmesh/catmesh.py
@@ -27,6 +27,8 @@ def CatchSelectedMesh(fileName):
     fmesh = om.MFnMesh(mesh.node())
     print('vertex count: %i' % fmesh.numVertices())
     
+    rec.write('static const int sNumVertices = %i;\n' % fmesh.numVertices())
+    
     triangleCounts = om.MIntArray()
     triangleVertices = om.MIntArray()
     
@@ -41,14 +43,12 @@ def CatchSelectedMesh(fileName):
         rec.write("%i, %i, %i,\n" % (triangleVertices[i * 3], triangleVertices[i * 3 + 1], triangleVertices[i * 3 + 2]))
     rec.write('};\n')
     
-    rec.write('static const int sNumVertices = %i;\n' % fmesh.numVertices())
-    
     vertexArray = om.MPointArray()
     fmesh.getPoints(vertexArray, om.MSpace.kObject)
     
     rec.write('static const float sMeshVertices[] = {')
     for i in range(0, vertexArray.length()):
-        rec.write("%f, %f, %f,\n" % (vertexArray[i].x, vertexArray[i].y, vertexArray[i].z))
+        rec.write("%ff, %ff, %ff,\n" % (vertexArray[i].x, vertexArray[i].y, vertexArray[i].z))
     rec.write('};\n')
     
     itv = om.MItMeshVertex(mesh);
@@ -57,7 +57,7 @@ def CatchSelectedMesh(fileName):
     while not itv.isDone():
         nor = om.MVector()
         itv.getNormal(nor, om.MSpace.kObject)
-        rec.write("%f, %f, %f,\n" % (nor.x, nor.y, nor.z))
+        rec.write("%ff, %ff, %ff,\n" % (nor.x, nor.y, nor.z))
         itv.next()
     rec.write('};\n')    
     rec.close()
