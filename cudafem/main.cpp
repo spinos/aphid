b52a24df39c075896db212d7c1d6cd3470058aab
zhangonmacbook <zhangmdev@gmail.com>
2015-07-31 03:43:05 +0800

    app icon extern resource

--- cudafem/main.cpp
+++ cudafem/main.cpp
@@ -9,7 +9,12 @@ int main(int argc, char *argv[])
 	std::cout<<"Cuda FEM start up\n";
 	if(argc > 1) FemGlobal::FileName = argv[argc - 1];
     else FemGlobal::FileName = "untitled.hes";
+// external binary resource
+// rcc -binary image.qrc -o resource.rcc
+    QResource::registerResource("resource.rcc");
     QApplication app(argc, argv);
+    QIcon professorIcon(":/professor.png");
+    app.setWindowIcon(professorIcon);
     Window window;
     //window.showMaximized();
     window.resize(800, 600);
