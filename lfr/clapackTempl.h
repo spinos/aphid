cf3d929b90df0e7598b6067b783d2803f7a2d4ed
zhangonmacbook <zhangmdev@gmail.com>
2015-11-18 17:47:43 +0800

    m sqrt

--- lfr/clapackTempl.h
+++ lfr/clapackTempl.h
@@ -9,6 +9,21 @@ template <typename T> void clapack_gemv(char * trans, integer M,  integer N,
        T alpha,  T *A,  integer lda,
        T* X,  integer incX,
 	   T beta, T * Y, integer incY);
+	   
+template <typename T> int clapack_gesvd(char *jobu, char *jobvt, integer m, integer n, 
+	T *a, integer lda, T *s, T *u, integer ldu, T *vt, 
+	integer ldvt, T *work, integer *lwork, integer *info);
+	
+template <typename T> int clapack_syevr(char *jobz, char *range, char *uplo, integer n, 
+	T *a, integer lda,
+	T *vl, T *vu, integer il, integer iu,
+	T abstol, integer *m,
+	T *w, T *z__, integer ldz, integer *isuppz, T *work, 
+	integer *lwork, integer *iwork, integer *liwork, integer *info);
+	
+template <typename T> int clapack_gemm(char *transa, char *transb, integer m, integer n, integer k, 
+	T alpha, T *a, integer lda, 
+	T *b, integer ldb, T beta, T *c__, integer ldc);
 
 template <> void clapack_axpy<double>(integer N,
        double alpha,  double* X,  integer incX,
@@ -42,4 +57,66 @@ template <> void clapack_gemv<float>(char * trans, integer M,  integer N,
            &alpha, A, &lda, 
            X, &incX, 
            &beta, Y, &incY);
+}
+
+template <> int clapack_gesvd<float>(char *jobu, char *jobvt, integer m, integer n, 
+	float *a, integer lda, float *s, float *u, integer ldu, float *vt, 
+	integer ldvt, float *work, integer *lwork, integer *info)
+{
+	return sgesvd_(jobu, jobvt, &m, &n, 
+		a,	&lda,	s,	u,	&ldu,	vt, 
+		&ldvt,	work,	lwork,	info);
+}
+
+template <> int clapack_gesvd<double>(char *jobu, char *jobvt, integer m, integer n, 
+	double *a, integer lda, double *s, double *u, integer ldu, double *vt, 
+	integer ldvt, double *work, integer *lwork, integer *info)
+{ return dgesvd_(jobu, jobvt, &m, &n, 
+		a,	&lda,	s,	u,	&ldu,	vt, 
+		&ldvt,	work,	lwork,	info); }
+		
+template <> int clapack_syevr<double>(char *jobz, char *range, char *uplo, integer n, 
+	double *a, integer lda,
+	double *vl, double *vu, integer il, integer iu,
+	double abstol, integer *m,
+	double *w, double *z__, integer ldz, integer *isuppz, double *work, 
+	integer *lwork, integer *iwork, integer *liwork, integer *info)
+{
+	return dsyevr_(jobz, range, uplo, &n, 
+	a, &lda, vl, vu, &il, &iu, 
+	&abstol, m, 
+	w, z__, &ldz, isuppz, work, 
+	lwork, iwork, liwork, info);
+}
+
+template <> int clapack_syevr<float>(char *jobz, char *range, char *uplo, integer n, 
+	float *a, integer lda, 
+	float *vl, float *vu, integer il, integer iu, 
+	float abstol, integer *m, 
+	float *w, float *z__, integer ldz, integer *isuppz, float *work, 
+	integer *lwork, integer *iwork, integer *liwork, integer *info)
+{
+	return ssyevr_(jobz, range, uplo, &n, 
+	a, &lda, vl, vu, &il, &iu, 
+	&abstol, m, 
+	w, z__, &ldz, isuppz, work, 
+	lwork, iwork, liwork, info);
+}
+
+template <> int clapack_gemm<double>(char *transa, char *transb, integer m, integer n, 
+	integer k, double alpha, double *a, integer lda, 
+	double *b, integer ldb, double beta, double *c__, integer ldc)
+{
+	return dgemm_(transa, transb, &m, &n, &k, 
+	&alpha, a, &lda, 
+	b, &ldb, &beta, c__, &ldc);
+}
+
+template <> int clapack_gemm<float>(char *transa, char *transb, integer m, integer n, 
+	integer k, float alpha, float *a, integer lda, 
+	float *b, integer ldb, float beta, float *c__, integer ldc)
+{
+	return sgemm_(transa, transb, &m, &n, &k, 
+	&alpha, a, &lda, 
+	b, &ldb,&beta, c__, &ldc);
 }
--- lfr/testlapack.cpp
+++ lfr/testlapack.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include "clapackTempl.h"
+#include <cmath>
 
-int main ( )
-{ 
-    int m = 5, n = 4;
+void printMatrix( char* desc, int m, int n, double* a ) 
+{
+	int i, j;
+	std::cout<<"\n "<<desc;
+	for(i=0; i< m; i++) {
+        std::cout<<"\n| ";
+        for(j=0; j< n; j++) {
+            std::cout<<" "<<a[j*m + i];
+        }
+        std::cout<<" |";
+    }
+	std::cout<<"\n";
+}
+
+void testAdd()
+{
+	std::cout<<"\n test axpy";
+	int m = 5, n = 4;
     double * A = new double[m*n];
     double * X = new double[m*n];
     int i, j;
@@ -16,34 +32,159 @@ int main ( )
         
     }
     
-    std::cout<<"\n A";
-    for(i=0; i< m; i++) {
-        std::cout<<"\n| ";
-        for(j=0; j< n; j++) {
-            std::cout<<" "<<A[j*m + i];
-        }
-        std::cout<<" |";
-    }
+    printMatrix("A", m, n, A);
     
-    std::cout<<"\n X";
-    for(i=0; i< m; i++) {
-        std::cout<<"\n| ";
-        for(j=0; j< n; j++) {
-            std::cout<<" "<<X[j*m + i];
-        }
-        std::cout<<" |";
-    }
+	printMatrix("X", m, n, X);
     
-    std::cout<<"\n A <- A + 0.5 X";
     clapack_axpy<double>(m*n, .5, X, 1, A, 1);
     
-    for(i=0; i< m; i++) {
-        std::cout<<"\n| ";
-        for(j=0; j< n; j++) {
-            std::cout<<" "<<A[j*m + i];
+	printMatrix("A <- A + 0.5 X", m, n, A);
+}
+
+#define M 6
+#define N 5
+#define LDA M
+#define LDU M
+#define LDVT N
+
+/// A = U*SIGMA*VT
+/// where SIGMA is an m-by-n matrix which is zero except for its min(m,n)
+/// diagonal elements, U is an m-by-m orthogonal matrix 
+/// and VT (V transposed) is an n-by-n orthogonal matrix. 
+/// The diagonal elements of SIGMA are the singular values of A; 
+/// they are real and non-negative, and are returned in descending order. The first min(m, n) columns of U and V are
+/// the left and right singular vectors of A.
+
+void testSVD()
+{
+	std::cout<<"\n test svd";
+	int m = M, n = N, lda = LDA, ldu = LDU, ldvt = LDVT;
+        double wkopt;
+        double* work;
+        /* Local arrays */
+        double s[N];
+		double u[LDU*M];
+		double vt[LDVT*N];
+        double a[LDA*N]= {
+            8.79,  6.11, -9.15,  9.57, -3.49,  9.84,
+            9.93,  6.91, -7.93,  1.64,  4.02,  0.15,
+            9.83,  5.04,  4.86,  8.83,  9.80, -8.99,
+            5.45, -0.27,  4.85,  0.74, 10.00, -6.02,
+            3.16,  7.98,  3.01,  5.80,  4.27, -5.31
+        };
+		/* Query and allocate the optimal workspace */
+        integer lwork = -1, info;
+        clapack_gesvd<double>( "All", "All", m, n, a, lda, s, u, ldu, vt, ldvt, &wkopt, &lwork, &info );
+        lwork = (int)wkopt;
+		std::cout<<"\n work l "<<lwork;
+        work = (double*)malloc( lwork*sizeof(double) );
+        /* Compute SVD */
+		clapack_gesvd<double>( "All", "All", m, n, a, lda, s, u, ldu, vt, ldvt, work, &lwork, &info );
+        /* Check for convergence */
+        if( info > 0 ) {
+                printf( "The algorithm computing SVD failed to converge.\n" );
+                exit( 1 );
         }
-        std::cout<<" |";
+        /* Print singular values */
+        printMatrix( "Singular values", 1, n, s );
+        /* Print left singular vectors */
+        printMatrix( "Left singular vectors (stored columnwise)", m, m, u );
+        /* Print right singular vectors */
+        printMatrix( "Right singular vectors (stored rowwise)", n, n, vt );
+        /* Free workspace */
+        free( (void*)work );
+}
+
+#define NSELECT 5
+void testSqrt()
+{
+	std::cout<<"\n todo test sqrt";
+	double A[N*N] = {
+            0.67,  0.00,  0.00,  0.00,  0.00,
+           -0.20,  3.82,  0.00,  0.00,  0.00,
+            0.19, -0.13,  3.27,  0.00,  0.00,
+           -1.06,  1.06,  0.11,  5.89,  0.00,
+            0.46, -0.48,  1.10, -0.98,  4.54
+        };
+	printMatrix("A", N, N, A);
+	
+	double W[N];
+	double Z[N*N];
+	integer ISUPPZ[N];
+	
+	double * work;// = (double *)malloc(26*N*sizeof(double));
+	integer * iwork;// = (integer *)malloc(10*N*sizeof(integer));
+	double abstol = -1.0;
+	double vl, vu;
+	int il = 1;
+	int iu = NSELECT;
+	integer m;
+	integer info;
+	integer lwork = -1;
+	integer liwork = -1;
+	double queryWork; work = &queryWork;
+	integer queryIwork; iwork = &queryIwork;
+	
+	clapack_syevr<double>("V", "A", "U", N, A, N, 
+		&vl, &vu, il, iu, abstol, &m,
+         W, Z, N, ISUPPZ, 
+		 work, &lwork, iwork, &liwork, &info);
+		 
+	std::cout<<"\n m "<<m;
+	std::cout<<"\n lwork "<<work[0];
+	std::cout<<"\n liwork "<<iwork[0];
+	std::cout<<"\n info "<<info;
+	
+	lwork = queryWork;
+	liwork = queryIwork;
+	std::cout<<"\n lwork "<<lwork;
+	std::cout<<"\n liwork "<<liwork;
+	
+	work = (double *)malloc(lwork*sizeof(double));
+	iwork = (integer *)malloc(liwork*sizeof(integer));
+	
+	clapack_syevr<double>("V", "A", "U", N, A, N, 
+		&vl, &vu, il, iu, abstol, &m,
+         W, Z, N, ISUPPZ, 
+		 work, &lwork, iwork, &liwork, &info);
+	if( info > 0 ) {
+                std::cout<<"\nThe algorithm failed to compute eigenvalues.\n";
+                exit( 1 );
+        }
+	std::cout<<"\n info "<<info;
+	std::cout<<"\n The total number of eigenvalues found: "<<m;
+	printMatrix("A", N, N, A);
+	printMatrix( "W", 1, m, W);
+	printMatrix( "Z", N, m, Z);
+	
+	double B[N*N];
+	int i, j;
+    for(i=0; i< N; i++) {
+		double  lambda=sqrt(W[i]);
+        for(j=0; j< N; j++) {
+            B[i*N + j] = Z[i*N + j] * lambda;
+        }
     }
-    
+	
+	printMatrix("B = Z * D", N, N, B);
+	
+	clapack_gemm<double>("N", "T", N, N, N,
+						1.0, B, N, Z, N, 0.0, A, N);
+						
+	printMatrix("A' = B * Zt", N, N, A);
+	
+	memcpy(B, A, N*N*sizeof(double));
+	
+	clapack_gemm<double>("N", "N", N, N, N,
+						1.0, A, N, B, N, 0.0, Z, N);
+						
+	printMatrix("A = A' * A'", N, N, Z);
+}
+
+int main()
+{ 
+    testAdd();
+    testSVD();
+	testSqrt();
     return 1;
 }
