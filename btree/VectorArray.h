3cd24fe9c644064628400617fce2367c02504797
zhangonmacbook <zhangmdev@gmail.com>
2016-02-22 06:03:06 +0800

    vec in blk

--- btree/VectorArray.h
+++ btree/VectorArray.h
@@ -15,34 +15,39 @@ namespace sdb {
 template <typename T>
 class VectorArray : public Entity {
 	
+	std::vector<T *> m_blocks;
 	std::vector<T *> m_data;
+	T * m_buf;
+	unsigned m_numData;
 	
 public:
-	VectorArray(Entity * parent = NULL) : Entity(parent) {}
+	VectorArray(Entity * parent = NULL) : Entity(parent),
+	m_numData(0) {}
 	
 	virtual ~VectorArray() 
 	{
-		const int n = m_data.size();
+		const int n = m_blocks.size();
 		int i = 0;
-		for(; i<n; i++) delete m_data[i];
-			
+		for(; i<n; i++) delete[] m_blocks[i];
+		m_blocks.clear();
 		m_data.clear();
 	}
 	
 	void insert(const T & a) 
 	{
-		T * d = new T;
+		if((m_numData & 4095)==0) {
+			m_buf = new T[4097];
+			m_blocks.push_back(m_buf);
+		}
+		
+		T * d = &m_buf[m_numData & 4095];
 		*d = a;
-		m_data.push_back(d); 
+		m_data.push_back(d);
+		m_numData++;
 	}
 	
-	void insert(T * d) 
-	{
-		m_data.push_back(d); 
-	}
-	
-	int size() const
-	{ return m_data.size(); }
+	const unsigned & size() const
+	{ return m_numData; }
 	
 	T * get(int idx) const
 	{ return m_data[idx]; }
