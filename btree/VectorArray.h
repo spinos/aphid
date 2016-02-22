ea1d7a5cc93124593d2ad55025fa37e7d3171819
zhangonmacbook <zhangmdev@gmail.com>
2016-02-23 00:04:42 +0800

    vecarr clear and access

--- btree/VectorArray.h
+++ btree/VectorArray.h
@@ -18,19 +18,35 @@ class VectorArray : public Entity {
 	std::vector<T *> m_blocks;
 	std::vector<T *> m_data;
 	T * m_buf;
-	unsigned m_numData;
+	int m_numData;
 	
 public:
 	VectorArray(Entity * parent = NULL) : Entity(parent),
 	m_numData(0) {}
 	
 	virtual ~VectorArray() 
+	{ clear(); }
+	
+	void clear()
 	{
+		m_numData = 0;
 		const int n = m_blocks.size();
 		int i = 0;
 		for(; i<n; i++) delete[] m_blocks[i];
 		m_blocks.clear();
 		m_data.clear();
+	}
+	
+	void insert()
+	{
+		if((m_numData & 4095)==0) {
+			m_buf = new T[4097];
+			m_blocks.push_back(m_buf);
+		}
+		
+		T * d = &m_buf[m_numData & 4095];
+		m_data.push_back(d);
+		m_numData++;
 	}
 	
 	void insert(const T & a) 
@@ -46,10 +62,16 @@ public:
 		m_numData++;
 	}
 	
-	const unsigned & size() const
+	const int & size() const
 	{ return m_numData; }
 	
 	T * get(int idx) const
+	{ return m_data[idx]; }
+	
+	T * operator[] (int idx)
+	{ return m_data[idx]; }
+	
+	const T * operator[] (int idx) const
 	{ return m_data[idx]; }
 };
 
