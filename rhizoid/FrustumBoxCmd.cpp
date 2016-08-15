3bde46ead1f5816878731eae2b8037842dd7ba26
zhangonmacbook <zhangmdev@gmail.com>
2016-08-15 12:50:33 +0800

    m

---
+++ rhizoid/FrustumBoxCmd.cpp
@@ -0,0 +1,387 @@
+/*
+ *  FrustumBoxCmd.cpp
+ *  FrustumBox
+ *
+ *  Created by jian zhang on 7/7/15.
+ *  Copyright 2015 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "FrustumBoxCmd.h"
+#include <ASearchHelper.h>
+
+#include <maya/MFnPointArrayData.h>
+#include <maya/MFnIntArrayData.h>
+#include <maya/MFnVectorArrayData.h>
+#include <maya/MFnAnimCurve.h>
+#include <maya/MItDependencyNodes.h>
+#include <GjkIntersection.h>
+#include <PrincipalComponents.h>
+
+using namespace aphid;
+
+FrustumBoxCmd::FrustumBoxCmd() {}
+FrustumBoxCmd::~FrustumBoxCmd() {}
+
+void* FrustumBoxCmd::creator()
+{
+	return new FrustumBoxCmd;
+}
+
+MSyntax FrustumBoxCmd::newSyntax() 
+{
+	MSyntax syntax;
+
+	syntax.addFlag("-h", "-help", MSyntax::kNoArg);
+    syntax.addFlag("-fr", "-frameRange", MSyntax::kLong, MSyntax::kLong);
+	syntax.addFlag("-cam", "-camera", MSyntax::kString);
+    syntax.addFlag("-lv", "-listVisible", MSyntax::kNoArg);
+	syntax.enableQuery(false);
+	syntax.enableEdit(false);
+
+	return syntax;
+}
+
+MStatus FrustumBoxCmd::parseArgs(const MArgList &args)
+{
+	MStatus			stat;
+	MArgDatabase	argData(syntax(), args, &stat);
+
+	if ( !stat )
+		return MS::kFailure;
+
+	m_mode = WHelp;
+	m_cameraName = "";
+    m_startTime = 1;
+    m_endTime = 24;
+    m_doListVisible = false;
+	
+    if(argData.isFlagSet("-fr")) {
+        m_mode = WAction;
+        stat = argData.getFlagArgument("-fr", 0, m_startTime);
+        stat = argData.getFlagArgument("-fr", 1, m_endTime);
+        if(!stat) {
+            MGlobal::displayWarning("invalid -fr argument");
+            m_mode = WHelp;
+        }
+    }
+	
+	if(argData.isFlagSet("-cam")) {
+        m_mode = WAction;
+        stat = argData.getFlagArgument("-cam", 0, m_cameraName);
+        if(!stat) {
+            MGlobal::displayWarning("invalid -cam argument");
+            m_mode = WHelp;
+        }
+    }
+    
+    if(argData.isFlagSet("-lv")) m_doListVisible = true;
+    
+	if(argData.isFlagSet("-h")) m_mode = WHelp;
+	
+	if(m_cameraName.length() < 3) {
+		MGlobal::displayWarning("invalid camera name");
+		m_mode = WHelp;
+	}
+	
+	return MS::kSuccess;
+}
+
+MStatus FrustumBoxCmd::doIt(const MArgList &argList)
+{
+	MStatus status;
+	status = parseArgs(argList);
+	if (!status)
+		return status;
+	
+	return redoIt();
+}
+
+MStatus	FrustumBoxCmd::redoIt()
+{
+	if(m_mode == WHelp) return printHelp();
+    
+    MObject ocamera;
+    ASearchHelper finder;
+    if( !finder.getObjByFullName(m_cameraName.asChar(), ocamera) ) {
+        MGlobal::displayWarning(MString("cannot find camera ")
+                                +m_cameraName);
+        return MS::kSuccess;
+    }
+    
+    MStatus stat;
+    MFnCamera fcam(ocamera, &stat);
+    if(!stat) {
+        MGlobal::displayWarning(MString("not a camera ")
+                                +m_cameraName);
+        return stat;
+    }
+    
+	MSelectionList selList;
+	MGlobal::getActiveSelectionList ( selList );
+	if(selList.length() < 1) {
+		MGlobal::displayWarning("empty selection");
+		return MS::kSuccess;
+	}
+    
+    MGlobal::displayInfo(MString("test camera frustum box intersection from ")
+                         + m_startTime
+                         + " to "
+                         + m_endTime);
+	
+    MDagPathArray paths;
+    MItSelectionList iter( selList );
+	for ( ; !iter.isDone(); iter.next() ) {								
+		MDagPath apath;		
+		iter.getDagPath( apath );
+        paths.append(apath);
+	}
+    
+    MGlobal::displayInfo(MString("n objects ") + paths.length());
+    std::vector<int> visibilities;
+    int i;
+    for(i=0; i< paths.length(); i++) visibilities.push_back(0);
+    
+    for(i=m_startTime; i<= m_endTime; i++) {
+        MGlobal::executeCommand(MString("currentTime ") + i);
+        collide(visibilities,
+                 paths,
+                 ocamera);
+        MGlobal::displayInfo(MString("frame") + i);
+        if(isAllVisible(visibilities)) {
+            MGlobal::displayInfo("all objects are visible");
+            break;
+        }
+    }
+
+    MGlobal::displayInfo(MString("done "));
+    
+    MStringArray names;
+    listObjects(names, paths, visibilities, m_doListVisible);
+    setResult(names);
+    
+	return MS::kSuccess;
+}
+
+MStatus FrustumBoxCmd::printHelp()
+{
+	MGlobal::displayInfo(MString("FrustumBox help info:\n test if objects are visible through a camera")
+		+MString("\n by intersecting object oriented box and camera frustum. \n example:")
+		+MString("\n select object(s) to test")
+		+MString("\n frustumBox -cam |persp|perspShape -fr 1 100")
+		+MString("\n -cam/-camera string is full path to camera shape")
+		+MString("\n -fr/-frameRange int int is begin and end frame of test")
+        +MString("\n -lv/-listVisible is set to return visible objects instead of invisible ones")
+		+MString("\n return value is string[] of objects NOT visible to camera"));
+	
+	return MS::kSuccess;
+}
+
+void FrustumBoxCmd::collide(std::vector<int> & visibilities,
+                 const MDagPathArray & paths,
+                 MObject & camera) const
+{
+    const unsigned n = paths.length();
+    Vector3F frustumA[8];
+    worldFrustum(frustumA, camera);
+    
+    gjk::IntersectTest test;
+    test.SetABox(frustumA);
+
+    gjk::BoxSet boxB;
+    unsigned i;
+    for(i=0; i< n; i++) {
+		worldBBox(boxB.x(), paths[i]);
+		if(test.evaluate(&boxB)) {
+/// test oriented box
+			worldOrientedBox(boxB.x(), paths[i]);
+			if(test.evaluate(&boxB)) {
+				// MGlobal::displayInfo("intersected"); 
+				visibilities[i] = 1;
+			}
+		}
+    }
+}
+
+void FrustumBoxCmd::getMeshTris(aphid::sdb::VectorArray<aphid::cvx::Triangle> & tris,
+								aphid::BoundingBox & bbox,
+								const MDagPath & meshPath) const
+{
+	aphid::AHelper::Info<MString>("get mesh triangles", meshPath.fullPathName() );
+	
+	MMatrix worldTm = aphid::AHelper::GetWorldParentTransformMatrix(meshPath);
+	
+    MStatus stat;
+	
+    MIntArray vertices;
+    int i, j, nv;
+	MPoint dp[3];
+	aphid::Vector3F fp[3];
+	MItMeshPolygon faceIt(meshPath);
+    for(; !faceIt.isDone(); faceIt.next() ) {
+
+		faceIt.getVertices(vertices);
+        nv = vertices.length();
+        
+        for(i=1; i<nv-1; ++i ) {
+			dp[0] = faceIt.point(0, MSpace::kObject );
+			dp[1] = faceIt.point(i, MSpace::kObject );
+			dp[2] = faceIt.point(i+1, MSpace::kObject );
+			
+			dp[0] *= worldTm;
+			dp[1] *= worldTm;	
+			dp[2] *= worldTm;
+			
+			aphid::cvx::Triangle tri;
+			for(j=0; j<3; ++j) {
+				fp[j].set(dp[j].x, dp[j].y, dp[j].z);
+				tri.setP(fp[j], j);
+				bbox.expandBy(fp[j], 1e-4f);
+			}
+			
+			tris.insert(tri);
+        }
+    }
+}
+
+void FrustumBoxCmd::worldOrientedBox(aphid::Vector3F * corners, const MDagPath & path) const
+{
+	MDagPathArray meshPaths;
+	ASearchHelper::LsAllTypedPaths(meshPaths, path, MFn::kMesh);
+	
+	const int n = meshPaths.length();
+	if(n < 1) {
+		AHelper::Info<MString>(" FrustumBoxCmd find no mesh in path, use world bbox", path.fullPathName() );
+		worldBBox(corners, path);
+		return;
+	}
+	
+	sdb::VectorArray<cvx::Triangle> tris;
+	BoundingBox bbox;
+	
+	int i=0;
+	for(;i<n;++i) 
+		getMeshTris(tris, bbox, meshPaths[i]);
+	
+	const int nt = tris.size();
+	if(nt < 1) {
+		AHelper::Info<MString>(" FrustumBoxCmd find no mesh triangle in path, use world bbox", path.fullPathName() );
+		worldBBox(corners, path);
+		return;
+	}
+	
+	std::vector<Vector3F> pnts;
+    for(i=0; i< nt; ++i) {
+        const aphid::cvx::Triangle * t = tris[i];
+/// at triangle center
+        pnts.push_back(t->P(0) * .33f 
+                       + t->P(1) * .33f
+                       + t->P(2) * .33f);
+    }
+	
+	tris.clear();
+	
+	PrincipalComponents<std::vector<Vector3F> > obpca;
+    AOrientedBox obox = obpca.analyze(pnts, pnts.size() );
+	
+	pnts.clear();
+	
+	obox.getBoxVertices(corners);
+	
+}
+
+void FrustumBoxCmd::worldBBox(Vector3F * corners, const MDagPath & path) const
+{
+    MBoundingBox res;
+    MStatus stat;
+    MFnDagNode fnode(path, &stat);
+    res = fnode.boundingBox();
+    
+    const MPoint pmin = res.min();
+    const MPoint pmax = res.max();
+    corners[0].set(pmin.x, pmin.y, pmin.z);
+    corners[1].set(pmax.x, pmin.y, pmin.z);
+    corners[2].set(pmin.x, pmax.y, pmin.z);
+    corners[3].set(pmax.x, pmax.y, pmin.z);
+    corners[4].set(pmin.x, pmin.y, pmax.z);
+    corners[5].set(pmax.x, pmin.y, pmax.z);
+    corners[6].set(pmin.x, pmax.y, pmax.z);
+    corners[7].set(pmax.x, pmax.y, pmax.z);
+    
+    MMatrix m = AHelper::GetWorldParentTransformMatrix(path);
+    Matrix44F mat;
+    AHelper::ConvertToMatrix44F(mat, m);
+    int i;
+    for(i=0; i<8; i++) corners[i] = mat.transform(corners[i]);
+    // for(i=0; i<8; i++) AHelper::Info<Vector3F>("boxB", corners[i]);
+}
+
+void FrustumBoxCmd::worldFrustum(Vector3F * corners, MObject & camera) const
+{
+    MStatus stat;
+    MFnCamera fcam(camera, &stat);
+    
+    const double nearClip = fcam.nearClippingPlane() + 0.001f;
+    const double farClip = fcam.farClippingPlane() - 0.001f;
+    const double hAperture = fcam.horizontalFilmAperture();
+    const double vAperture = fcam.verticalFilmAperture();
+    const double fl = fcam.focalLength();
+    
+    float h_fov = hAperture * 0.5 / ( fl * 0.03937 );
+    float v_fov = vAperture * 0.5 / ( fl * 0.03937 );
+
+    float fright = farClip * h_fov;
+    float ftop = farClip * v_fov;
+
+    float nright = nearClip * h_fov;
+    float ntop = nearClip * v_fov;
+
+    corners[0].set(fright, ftop, -farClip);
+	corners[1].set(-fright, ftop, -farClip);
+	corners[2].set(-fright, -ftop, -farClip);
+	corners[3].set(fright, -ftop, -farClip);
+	corners[4].set(nright, ntop, -nearClip);
+	corners[5].set(-nright, ntop, -nearClip);
+	corners[6].set(-nright, -ntop, -nearClip);
+	corners[7].set(nright, -ntop, -nearClip);
+    
+    MDagPath pcam;
+    MDagPath::getAPathTo(camera, pcam);
+    
+    MMatrix m = AHelper::GetWorldTransformMatrix(pcam);
+    Matrix44F mat;
+    AHelper::ConvertToMatrix44F(mat, m);
+    
+    // AHelper::Info<Matrix44F>("cammat", mat);
+    
+    int i;
+    for(i=0; i<8; i++) corners[i] = mat.transform(corners[i]);
+    // for(i=0; i<8; i++) AHelper::Info<Vector3F>("frustumA", corners[i]);
+}
+
+bool FrustumBoxCmd::isAllVisible(const std::vector<int> & visibilities) const
+{
+    unsigned i;
+    const unsigned n = visibilities.size();
+    for(i=0; i< n; i++) {
+        if(visibilities[i] == 0) return false;   
+    }
+    return true;
+}
+
+void FrustumBoxCmd::listObjects(MStringArray & dst,
+                 const MDagPathArray & paths,
+                 const std::vector<int> & visibilities,
+                 bool visible)
+{
+    int k = 0;
+    if(visible) k = 1;
+    unsigned i;
+    const unsigned n = visibilities.size();
+    for(i=0; i< n; i++) {
+        if(visibilities[i] == k) {
+            dst.append(paths[i].fullPathName());
+        }
+    }
+}
+//:~
