64f364f79e365540411132d0d6403c7afe4d4b40
zhangonmacbook <zhangmdev@gmail.com>
2016-08-21 20:11:06 +0800

    trapz integral and normalize

--- tet/LegendreTest.cpp
+++ tet/LegendreTest.cpp
@@ -21,6 +21,16 @@
 using namespace aphid;
 namespace ttg {
 
+const float LegendreTest::NormWeightF[POLY_MAX_DEG+1] = {
+1.f,
+1.f,
+.6666667f,
+.4f,
+.22857143f,
+.12698413f,
+.06926407f
+};
+
 LegendreTest::LegendreTest() 
 {}
 
@@ -30,16 +40,27 @@ LegendreTest::~LegendreTest()
 const char * LegendreTest::titleStr() const
 { return "Legendre Polynomial Approximation Test"; }
 
+float LegendreTest::evaluateExact(const float & x) const
+{ 
+	//return 10.f - x* 2.f + x * x / 10.f;
+	//return x*x*x*x - x*x * 2 / 3 + 1.0 / 9.0;
+	return x*x*x - x * 3.0 / 5.0;
+	return x*x*x*x*x*x - x*x*x*x * 6.0 / 5.0 + x*x * 9.0 / 25.0;
+	return (x*x - 1.0/3.0);
+	return exp(x) * x; 
+}
+
 bool LegendreTest::init()
 {
 	int i;
 	for(i=0;i<NUM_SAMPLE;++i) {
-		m_exactEvaluate[i] = evaluateExact(DX_SAMPLE * i);
+		m_exactEvaluate[i] = evaluateExact(INTERVAL_A + DX_SAMPLE * i);
 	}
 	
-	computeLegendrePoly();
+	computeLegendrePolynomials();
+	computePipi();
 	
-	int m = 2, n = 5;
+	int m = 2, n = 257;
 	std::cout<<"\n deg "<<m<<" order "<<n;
 	
 	equalSpacingNodes(n, m_x);
@@ -48,28 +69,84 @@ bool LegendreTest::init()
 		m_y[i] = evaluateExact(m_x[i]);
 	}
 	
-	printValues("X", n, m_x);
-	printValues("Y", n, m_y);
-	
+	//printValues("X", n, m_x);
+	//printValues("Y", n, m_y);
+			
 	//legendreValue(m, n, m_x, m_v);
-	//computePipi(m_pipi, m, n, m_v);
 	
-	computePix(m_v, m_pipi, m_xpipi, m, n, m_x);
-	computeCoeff(m_coeff, m, n, m_y, m_v, m_pipi);
-	computeApproximated(m_approximated, m, m_coeff);
+	//computePix(m_v, m_pipi, m_xpipi, m, n, m_x);
+	//computeCoeff(m_coeff, m, n, m_y, m_v, m_pipi);
+	
+	//computeApproximated(m_approximateEvaluate, m, m_coeff);
 	
 	std::cout<<"\n done!";
 	std::cout.flush();
 	return true;
 }
 
+void LegendreTest::computeLegendrePolynomials()
+{
+	evaluateLegendre(NUM_SAMPLE, POLY_MAX_DEG, m_v);
+}
+
+void LegendreTest::evaluateLegendre(int m, int n, float * v) const
+{
+  int i;
+  int j;
+
+  for ( i = 0; i < m; i++ )
+  {
+    v[i+0*m] = 1.0;
+  }
+
+  for ( i = 0; i < m; i++ )
+  {
+    v[i+1*m] = INTERVAL_A + DX_SAMPLE * i;
+  }
+ 
+  for ( j = 2; j <= n; j++ )
+  {
+    for ( i = 0; i < m; i++ )
+    {
+      v[i+j*m] = ( ( float ) ( 2 * j - 1 ) * (INTERVAL_A + DX_SAMPLE * i) * v[i+(j-1)*m]   
+                 - ( float ) (     j - 1 ) *        v[i+(j-2)*m] ) 
+                 / ( float ) (     j     );
+    }
+  }
+}
+
+void LegendreTest::computePipi()
+{
+	float px2[NUM_SAMPLE];
+	int i, j;
+	for(i=0; i<=POLY_MAX_DEG; ++i) {
+		for(j=0;j<NUM_SAMPLE; ++j) {
+			px2[j] = m_v[NUM_SAMPLE * i + j]  * NormWeightF[i]; /// normalize
+			px2[j] *= px2[j]; /// <pi, pi>
+		}
+		
+		m_pipi[i] = trapezIntegral(INTERVAL_A, INTERVAL_B, NUM_SAMPLE, px2);
+			
+		std::cout<<"\n <p"<<i<<",p"<<i<<"> "<<m_pipi[i];
+	}
+	
+#if 0
+	std::cout<<"\n test orthogonality";
+	
+	for(j=0;j<NUM_SAMPLE; ++j) {
+			px2[j] = m_v[NUM_SAMPLE * 1 + j]; /// normalize
+			px2[j] *= m_v[NUM_SAMPLE * 2 + j]; /// <pi, pi>
+	}
+	
+	std::cout<<"\n <p2,p1> "<<trapezIntegral(INTERVAL_A, INTERVAL_B, NUM_SAMPLE, px2);
+#endif
+
+}
+
 void LegendreTest::computePix(float * m_v, float * pipi, float * xpipi,
 					int m, int n, const float * x) const
 {
 	int i, k;
-	for(k=0; k<=m;++k) {
-		pipi[k] = xpipi[k] = 0.f;
-	}
 	
 	float pm1 = 0.f, pm2 = 0.f;
 	for(k=0; k<=m;++k) {
@@ -101,7 +178,7 @@ float LegendreTest::computePi(int k, const float * pipi, const float * xpipi, co
 		return 1.f;
 		
 	if(k==1)
-		return (x - xpipi[0] / pipi[0]);
+		return x - xpipi[0] / pipi[0];
 /// orthogonal polynomial sequence		
 	return (x - xpipi[k-1] / pipi[k-1] ) * pm1 - pipi[k-1] / pipi[k-2] * pm2;
 	
@@ -129,40 +206,24 @@ float LegendreTest::integrateXpipi(const float * v, const float * x, int k, int 
 
 void LegendreTest::equalSpacingNodes(int n, float * x) const
 {
-	float dx = 1.f / (float)(n-1);
+	float dx = (INTERVAL_B - INTERVAL_A) / (float)(n-1);
 	int i=0;
 	for(;i<n;++i)
-		x[i] = dx * i;
+		x[i] = INTERVAL_A + dx * i;
 }
 
-void LegendreTest::computeLegendrePoly()
-{
-	float pj0 = 0.f, pj1 = 0.f;
-	int i, j;
-	
-	for(i=0;i<NUM_SAMPLE;++i) {
-		for(j=0; j<=POLY_MAX_DEG; ++j) {
-			if(j>1) {
-				pj0 = m_ps[j-2][i];
-				pj1 = m_ps[j-1][i];
-			}
-			m_ps[j][i] = legendreValue1(j, DX_SAMPLE * i, pj1, pj0);
-		}
-	}
-}
-
-float LegendreTest::legendreValue1(int m, const float & x,
+float LegendreTest::legendreValue1(int k, const float & x,
 									const float & pxm1, const float & pxm2) const
 {
-	if(m==0)
+	if(k==0)
 		return 1.f;
 		
-	if(m==1)
-		return 2.0 * x - 1.0;
+	if(k==1)
+		return x;//2.0 * x - 1.0;
 		
-	return ( ( float ) ( 2 * m - 1 ) * ( 2.0 * x - 1.0 ) * pxm1   
-                 - ( float ) (     m - 1 ) *              pxm2 ) 
-                 / ( float ) (     m     );
+	return ( ( float ) ( 2 * k - 1 ) * x * pxm1//( 2.0 * x - 1.0 ) * pxm1   
+                 - ( float ) (     k - 1 ) *              pxm2 ) 
+                 / ( float ) (     k     );
 }
 
 void LegendreTest::legendreValue(int m, int n, const float * x, float * v) const
@@ -172,43 +233,12 @@ void LegendreTest::legendreValue(int m, int n, const float * x, float * v) const
 	for(i=0;i<n;++i) {
 		for(j=0; j<=m; ++j) {
 			if(j>1) {
-				pj0 = m_ps[j-2][i];
-				pj1 = m_ps[j-1][i];
+				pj0 = v[i*(m+1) + j - 2];
+				pj1 = v[i*(m+1) + j - 1];
 			}
 			v[i*(m+1) + j] = legendreValue1(j, x[i], pj1, pj0);
-			std::cout<<"\n "<<j<<" "<<i<<" "<<v[i*(m+1) + j];
+			std::cout<<"\n p("<<j<<", x)["<<i<<"] "<<v[i*(m+1) + j];
 		}
-	}
-/*	
-/// l 0
-	for ( i = 0; i < m; i++ ) {
-		v[i+0*m] = 1.0;
-	}
-/// l 1
-	for ( i = 0; i < m; i++ ) {
-		v[i+1*m] = 2.0 * x[i] - 1.0;
-	}
-/// l 2, 3, ...	
-	for ( j = 2; j <= n; j++ ) {
-		for ( i = 0; i < m; i++ ) {
-			v[i+j*m] = ( ( float ) ( 2 * j - 1 ) * ( 2.0 * x[i] - 1.0 ) * v[i+(j-1)*m]   
-                 - ( float ) (     j - 1 ) *                        v[i+(j-2)*m] ) 
-                 / ( float ) (     j     );
-		}
-	}*/
-}
-
-void LegendreTest::computePipi(float * pipi, int m, int n, const float * v) const
-{
-	int i, j;
-	for(i=0; i<=m; ++i) {
-		pipi[i] = 0.f;
-		for(j=0; j<n; ++j) {
-			std::cout<<"\n "<<i<<" "<<j<<" "<<v[j*(m+1)+i];
-			pipi[i] += v[j*(m+1)+i] * v[j*(m+1)+i];
-		}
-			
-		std::cout<<"\n <p"<<i<<",p"<<i<<"> "<<pipi[i];
 	}
 }
 
@@ -219,26 +249,40 @@ void LegendreTest::computeCoeff(float * coeff, int m, int n, const float * y, co
 		coeff[i] = 0.f;
 		for(j=0; j<n; ++j) {
 			coeff[i] += y[j] * v[j*(m+1)+i];
+			
 		}
 		
 		coeff[i] /= pipi[i];
+		
 		std::cout<<"\n c"<<i<<" "<<coeff[i];
 	}
 }
 
 void LegendreTest::computeApproximated(float * yhat, int m, const float * coeff) const
 {
-	float x, xp;
+/// temporary
+	float pm[POLY_MAX_DEG+1];
+	float pm1 = 0.f, pm2 = 0.f;
+	
+	float x;
 	int i, j;
 	for(i=0;i<NUM_SAMPLE;++i) {
-		x = (DX_SAMPLE * i - 0.5f);
-		xp = 1.f;
+		x = INTERVAL_A + DX_SAMPLE * i;
+		
+		for(j=0; j<=m; ++j) {
+			if(j>1) {
+				pm2 = pm[j-2];
+				pm1 = pm[j-1];
+			}
+			pm[j] = legendreValue1(j, x, pm1, pm2);
+			
+		}
 		
 		yhat[i] = 0.f;
 		for(j=0; j<=m; ++j) {
-			yhat[i] += coeff[j] * xp;
+			yhat[i] += coeff[j] * pm[j];
 			
-			xp *= x;
+			
 		}
 	}
 }
@@ -249,23 +293,17 @@ void LegendreTest::draw(GeoDrawer * dr)
 	drawEvaluate(m_exactEvaluate, dr);
 	
 	glColor3f(0.33f, 1.f, 0.33f);
-	drawEvaluate(m_approximated, dr);
+	//drawEvaluate(m_approximateEvaluate, dr);
 
 #if 1
-	int i=0;
 	Vector3F mcol;
+	int i=0;
 	for(;i<=POLY_MAX_DEG;++i) {
 		sdb::gdt::GetCellColor(mcol, i);
 		glColor3fv((const float *)&mcol);
 		drawLegendrePoly(i, dr);
 	}
 #endif
-}
-
-float LegendreTest::evaluateExact(const float & x) const
-{ 
-	return 2.f - x* .1f - x * x / 3.f;
-//	return exp(x * .845f) - .34f * sqrt(x); 
 }
 
 void LegendreTest::printValues(const char * h, int n, const float * y) const
@@ -314,12 +352,12 @@ float LegendreTest::discreteIntegral(int n, const float * y, const float * v) co
 void LegendreTest::drawEvaluate(const float * y, GeoDrawer * dr)
 {
 	glPushMatrix();
-	glScaled(20.0, 10.0, 10.0);
+	glScaled(15.0, 10.0, 10.0);
 	glBegin(GL_LINES);
 	int i;
 	for(i=0;i<NUM_SAMPLE-1;++i) {
-		glVertex3f(DX_SAMPLE * i, y[i], 0.f);
-		glVertex3f(DX_SAMPLE * (i+1), y[i+1], 0.f);
+		glVertex3f(INTERVAL_A + DX_SAMPLE * i, y[i], 0.f);
+		glVertex3f(INTERVAL_A + DX_SAMPLE * (i+1), y[i+1], 0.f);
 	}
 	glEnd();
 	glPopMatrix();
@@ -328,15 +366,26 @@ void LegendreTest::drawEvaluate(const float * y, GeoDrawer * dr)
 void LegendreTest::drawLegendrePoly(int m, GeoDrawer * dr)
 {
 	glPushMatrix();
-	glScaled(20.0, 10.0, 10.0);
+	glScaled(15.0, 10.0, 10.0);
 	glBegin(GL_LINES);
 	int i;
 	for(i=0;i<NUM_SAMPLE-1;++i) {
-		glVertex3f(DX_SAMPLE * i, m_ps[m][i], 0.f);
-		glVertex3f(DX_SAMPLE * (i+1), m_ps[m][i+1], 0.f);
+		glVertex3f(INTERVAL_A + DX_SAMPLE * i, m_v[NUM_SAMPLE*m + i ], 0.f);
+		glVertex3f(INTERVAL_A + DX_SAMPLE * (i+1), m_v[NUM_SAMPLE*m + i +1], 0.f);
 	}
 	glEnd();
 	glPopMatrix();
+}
+
+float LegendreTest::trapezIntegral(const float & a, const float & b, int m, const float * y) const
+{
+	const float h = (b - a) / (m - 1);
+	float c = h * .5f * (y[0] + y[m-1]);
+	int i=1;
+	for(;i<m-1;++i) {
+		c += h * y[i];
+	}
+	return c;
 }
 
 }
--- tet/LegendreTest.h
+++ tet/LegendreTest.h
@@ -13,25 +13,29 @@ namespace ttg {
 
 class LegendreTest : public Scene {
 
-#define NUM_SAMPLE 65
-#define DX_SAMPLE 0.015625f
-#define POLY_MAX_DEG 6
-#define POLY_MAX_EVA 10
+#define NUM_SAMPLE 257  /// m, the number of evaluation points
+#define POLY_MAX_DEG 6  /// n, the highest order polynomial to evaluate
+#define INTERVAL_A -1.f
+#define INTERVAL_B  1.f
+#define DX_SAMPLE 0.0078125f
 
-	float m_exactEvaluate[NUM_SAMPLE];
-	float m_approximated[NUM_SAMPLE];
 /// each evaluate has 0 - m th polynomial
-	float m_v[(POLY_MAX_DEG+1) * POLY_MAX_EVA];
-	float m_coeff[POLY_MAX_DEG+1];
-	float m_x[POLY_MAX_EVA];
-	float m_y[POLY_MAX_EVA];
+	float m_v[NUM_SAMPLE * (POLY_MAX_DEG+1)];
 /// integral of pi(x)pi(x)
 	float m_pipi[POLY_MAX_DEG+1];
+		
+	float m_exactEvaluate[NUM_SAMPLE];
+	float m_approximateEvaluate[NUM_SAMPLE];
+
+	float m_coeff[POLY_MAX_DEG+1];
+	float m_x[NUM_SAMPLE];
+	float m_y[NUM_SAMPLE];
+
 /// integral of xpi(x)pi(x)
 	float m_xpipi[POLY_MAX_DEG+1];
-/// polynomial samples
-	float m_ps[POLY_MAX_DEG+1][NUM_SAMPLE];
-
+	
+	static const float NormWeightF[POLY_MAX_DEG+1];
+	
 public:
 	LegendreTest();
 	virtual ~LegendreTest();
@@ -42,8 +46,12 @@ public:
 	
 private:
 	void equalSpacingNodes(int n, float * x) const;
-	void computeLegendrePoly();
-	float legendreValue1(int m, const float & x, const float & pxm1, const float & pxm2) const;
+	void computeLegendrePolynomials();
+/// integral <pi, pi>
+	void computePipi();
+	
+	void evaluateLegendre(int m, int n, float * v) const;
+	float legendreValue1(int k, const float & x, const float & pxm1, const float & pxm2) const;
 	
 	float evaluateExact(const float & x) const;
 /// m max degree of polynomial highest x^m
@@ -57,15 +65,21 @@ private:
 	void printValues(const char * h, int n, const float * y) const;
 	void drawEvaluate(const float * y, aphid::GeoDrawer * dr);
 	void drawLegendrePoly(int m, aphid::GeoDrawer * dr);
-	void computePipi(float * pipi, int m, int n, const float * v) const;
+	
 	void computeCoeff(float * coeff, int m, int n, const float * y, const float * v, const float * pipi) const;
 	void computeApproximated(float * yhat, int m, const float * coeff) const;
+/// http://mathworld.wolfram.com/Gram-SchmidtOrthonormalization.html
+/// eq 24 recurrence relation
 	void computePix(float * m_v, float * pipi, float * xpipi,
 					int m, int n, const float * x) const;
 	float computePi(int k, const float * pipi, const float * xpipi, const float & x,
 					const float & pm1, const float & pm2) const;
 	float integratePipi(const float * v, int k, int m, int n) const;
 	float integrateXpipi(const float * v, const float * x, int k, int m, int n) const;
+/// http://mathfaculty.fullerton.edu/mathews/n2003/TrapezoidalRuleMod.html
+/// finding the area under a curve y = f(x) over [a, b] with m subintervals x0, x1, xm-1
+/// x0 = a, xm-1 = b
+	float trapezIntegral(const float & a, const float & b, int m, const float * y) const;
 	
 };
 
