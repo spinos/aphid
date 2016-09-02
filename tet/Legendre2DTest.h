a091f18f0df44400fcec3d90657291bb54ee7458
zhangonmacbook <zhangmdev@gmail.com>
2016-09-02 21:02:12 +0800

    m

--- tet/Legendre2DTest.h
+++ tet/Legendre2DTest.h
@@ -15,7 +15,18 @@ namespace ttg {
 class Legendre2DTest : public Scene {
 
 	aphid::AQuadMesh m_exact;
-	
+	aphid::AQuadMesh m_appro;
+#define N_DIM 2
+#define N_ORD 3
+#define N_ORD2 9
+#define N_P 2
+	float m_Wi[N_ORD];
+	float m_Xi[N_ORD];
+	float m_Pv[N_ORD * (N_P+1)];
+	float m_Pii[N_P+1];
+	float m_Yij[N_ORD2];
+	float m_Coeij[(N_P+1)*(N_P+1)];
+
 public:
 	Legendre2DTest();
 	virtual ~Legendre2DTest();
@@ -25,7 +36,12 @@ public:
 	virtual void draw(aphid::GeoDrawer * dr);
 	
 private:
-
+	float exactMeasure(const float & x, const float & y) const;
+/// l-th x m-th y coefficient by integrate f(x,y)P(l,x)P(m,y)
+	float computeCoeff(int l, int m) const;
+/// continuous function expressed as a linear combination of Legendre polynomials
+	float approximate(const float & x, const float & y) const;
+	
 };
 
 }
