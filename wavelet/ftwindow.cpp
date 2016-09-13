925b291508166a587f86548ae6a41452cac2bb51
td05 <td05@TD05.of3d.com>
2016-09-14 03:42:28 +0800

    a sho filter

---
+++ wavelet/ftwindow.cpp
@@ -0,0 +1,22 @@
+#include <QtGui>
+
+#include "ftwindow.h"
+
+//! [0]
+Window::Window()
+{
+    qDebug()<<"window";
+    //glWidget = new GLWidget;
+	
+	//setCentralWidget(glWidget);
+    setWindowTitle(tr("Untitled"));
+}
+//! [1]
+
+void Window::keyPressEvent(QKeyEvent *e)
+{
+	if (e->key() == Qt::Key_Escape)
+        close();
+
+	QWidget::keyPressEvent(e);
+}
